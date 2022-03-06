43e9539d44240e71f745fbf7dc8a08443f25b613
anas_aijaz <cjphonehomec@gmail.com>
2022-03-06 21:35:17 +0530

    + Added Bodoni Moda font

---
+++ src/components/Appbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar, Toolbar} from "@mui/material";
+
+const Appbar = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Appbar;
